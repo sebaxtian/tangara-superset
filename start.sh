ad7c66237a8ce845cf5b83c642f36d8610bb1c2b
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2023-12-05 09:23:22 -0800

    Fixing minor issues

--- start.sh
+++ start.sh
@@ -20,6 +20,7 @@ else
     SECRET_KEY=$(openssl rand -base64 42)
     echo "SECRET_KEY = '$SECRET_KEY'" >> $SUPERSET_CONFIG
     echo "PREVENT_UNSAFE_DB_CONNECTIONS = False" >> $SUPERSET_CONFIG
+    echo "TALISMAN_ENABLED = False" >> $SUPERSET_CONFIG
 fi
 
 # Environment Variable SUPERSET_CONFIG_PATH
@@ -29,4 +30,4 @@ export SUPERSET_CONFIG_PATH=$PWD/superset_config.py
 export FLASK_APP=superset
 
 # Stating Superset development mode
-superset run -p 8088 --with-threads --reload --debugger
+superset run -h 0.0.0.0 -p 8088 --with-threads --reload --debugger
